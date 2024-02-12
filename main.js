99b6f77a3ce1766124048d2ee38a2ac43d2f384c
Seungho Shin <nsmusic80@naver.com>
2024-02-13 01:18:37 +0900

    style

--- main.js
+++ main.js
@@ -7,6 +7,8 @@
 /// 5번의 기회. 다 쓰면 버튼 비활성화
 /// 1~100 범위 밖의 숫자 입력시 알려주고 라이프는 유지
 /// 입력한 숫자를 다시 입력시 알려주고 라이프 유지
+//// 한 번에 맞췄을 경우 라이프가 줄어들지 않아야 하고 실행 버튼이 비활성화 : 버튼 비활성 추가
+//// 1번의 기회가 남았을 때 정답을 입력해도 죽어버리는 경우 수정 : userLife -- 를 if문 아래로 옮겨서 해결
 
 let comNum = 0
 let playBtn = document.getElementById("play_btn")
@@ -16,6 +18,7 @@ let resetBtn = document.getElementById("reset_btn")
 let userLife = 5
 let gameOver = false
 let lifeLeft = document.getElementById("lifeLeft")
+let history = []
 
 playBtn.addEventListener("click", play);
 resetBtn.addEventListener("click", reset)
@@ -27,15 +30,31 @@ function pickRandomNum() {
 
 function play() {
     let userValue = userInput.value
-    userLife --
-    lifeLeft.textContent = `Your Life : ${userLife}`
+
+    if (userValue<1 || userValue>100) {
+        resultArea.textContent = "Choice a number of 1 to 100"
+        return
+    } else if (history.includes(userValue)) {
+        resultArea.textContent = "You forgot you did. That number has used"
+        return
+    }
+    
     if (userValue < comNum) {
         resultArea.textContent = "Up!!!"
     } else if (userValue >comNum) {
         resultArea.textContent = "Down!!!"
     } else {
         resultArea.textContent = "Correct...But you took something for granted."
+        lifeLeft.textContent = ""
+        playBtn.disabled = true
+        return
     }
+
+    userLife --
+    lifeLeft.textContent = `Your Life : ${userLife}`
+
+    history.push(userValue)
+
     if (userLife < 1) {
         gameOver = true
     }
@@ -51,7 +70,7 @@ function reset() {
     gameOver = false
     playBtn.disabled = false
     pickRandomNum()
-    resultArea.textContent = "결과를 여기서 확인하시오"
+    resultArea.textContent = "Result is here"
     lifeLeft.textContent = `Your Life : ${userLife}`
 }
 
